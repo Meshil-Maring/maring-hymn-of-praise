9e9e9754a3ba6ad89498ef076de9244ee4be169e
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-09-26 21:42:05 +0530

    complete working on categories

--- client/src/component/nav/header/Category.tsx
+++ client/src/component/nav/header/Category.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import CategoryList from "./CategoryList";
 
-const list = ["Numerical", "Alphabetical", "Christmas"];
+const list = {
+  number: "Numerical",
+  alphabet: "Alphabetical",
+  christmas: "Christmas",
+};
 
-const Category = () => {
+interface CategoryProps {
+  activeHandler: (params: { id: string | number }) => void;
+}
+
+const Category = ({ activeHandler }: CategoryProps) => {
+  const [active, setActive] = useState("number");
+
+  const clickHandler = ({ id }: { id: string | number }) => {
+    setActive(String(id));
+
+    activeHandler({ id: id });
+  };
+
   return (
-    <ul className={`p-2 flex scroll-auto gap-3`}>
-      {list.map((ele) => (
-        <CategoryList key={ele} active={ele === "Numerical"} name={ele} />
+    <ul className="p-2 flex gap-3 w-full overflow-x-auto">
+      {Object.entries(list).map(([key, value]) => (
+        <li key={key}>
+          <button onClick={() => clickHandler({ id: key })}>
+            <CategoryList name={value} active={active === String(key)} />
+          </button>
+        </li>
       ))}
     </ul>
   );
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,15 @@
 import Header from "../component/nav/header/Header";
 import Category from "../component/nav/header/Category";
 
+const activeHandler = ({ id }: { id: String | Number }) => {
+  console.log(String(id));
+};
+
 const Home = () => {
   return (
     <div className="bg-bg-light">
       <Header />
-      <Category />
+      <Category activeHandler={activeHandler} />
     </div>
   );
 };
