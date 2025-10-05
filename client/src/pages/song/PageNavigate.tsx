5dbdaecb509ea873c1522dfc2d458f01f8707830
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-05 22:58:01 +0530

    add page negivator next and previous button

--- client/src/pages/song/PageNavigate.tsx
+++ client/src/pages/song/PageNavigate.tsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
 import RightIcon from "../../assets/icons/right";
 import LeftIcon from "../../assets/icons/left";
 
-const PageNavigate = () => (
-  <div className="flex gap-6 mx-auto items-center mb-2">
-    <button className="bg-yellow p-2 rounded-full">
-      <LeftIcon />
-    </button>
+const PageNavigate = () => {
+  const [index, setIndex] = useState<number>(1);
 
-    <div className="flex items-center w-full">
-      <p className="bg-bg-light px-4 text-blue-300 py-1 text-center rounded-l-lg">
-        1
-      </p>
-      <p className="bg-active text-white flex w-10 h-10 justify-center items-center rounded-lg text-2xl font-bold">
-        2
-      </p>
-      <p className="bg-bg-light px-4 text-blue-300 py-1 text-center rounded-r-lg">
-        3
-      </p>
+  //   Handling next and previous button
+  const clickHandler = (value: string) => {
+    if (value === "next" && index < 461) {
+      setIndex(index + 1);
+    } else if (value === "prev" && index > 1) {
+      setIndex(index - 1);
+    }
+  };
+
+  return (
+    <div className="flex gap-6 mx-auto items-center mb-2">
+      <button
+        onClick={() => clickHandler("prev")}
+        className="bg-yellow p-2 rounded-full"
+      >
+        <LeftIcon />
+      </button>
+
+      <div className="flex items-center w-full">
+        <p
+          className={`${
+            index == 1
+              ? "bg-transparent text-transparent"
+              : "bg-bg-light text-blue-300"
+          }  px-4  py-1 text-center rounded-l-lg`}
+        >
+          {index - 1}
+        </p>
+
+        <p className="bg-active text-white flex w-10 h-10 justify-center items-center rounded-lg text-xl font-medium">
+          {index}
+        </p>
+
+        <p
+          className={`${
+            index == 461
+              ? "bg-transparent text-transparent"
+              : "bg-bg-light text-blue-300"
+          }  px-4  py-1 text-center rounded-r-lg`}
+        >
+          {index + 1}
+        </p>
+      </div>
+
+      <button
+        onClick={() => clickHandler("next")}
+        className="bg-yellow p-2 rounded-full"
+      >
+        <RightIcon size={24} />
+      </button>
     </div>
-
-    <button className="bg-yellow p-2 rounded-full">
-      <RightIcon size={24} />
-    </button>
-  </div>
-);
+  );
+};
 
 export default PageNavigate;
