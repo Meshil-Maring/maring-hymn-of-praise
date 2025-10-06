befc69c84d153493f852bd5617da058b37f47b8f
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-06 23:00:29 +0530

    setup typescript in nodejs (server side)

---
+++ server/app.ts
@@ -0,0 +1,28 @@
+import express, { Application } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import mainRoutes from "./src/routes/main-routes";
+
+const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// 🛣️ Routes
+app.use("/", mainRoutes);
+
+// Mongodb connection
+mongoose
+  .connect(
+    "mongodb+srv://dsmeshilmaring13_db_user:7G4IAenOiglH0csG@cluster0.hgdnbwo.mongodb.net/test" // change DB name if needed
+  )
+  .then(() => {
+    console.log("✅ MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
