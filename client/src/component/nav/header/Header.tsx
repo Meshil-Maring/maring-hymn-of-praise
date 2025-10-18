c01ca1190b7a87ee4018657e2dcde3842ac6bdf1
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-18 12:04:26 +0530

    change input type

--- client/src/component/nav/header/Header.tsx
+++ client/src/component/nav/header/Header.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import MenuIcon from "../../../assets/icons/menu";
 import SearchIcon from "../../../assets/icons/search";
 import StarIcon from "../../../assets/icons/star";
 import CloseIcon from "../../../assets/icons/close";
+import ChangeToAIcon from "../../../assets/icons/changeToA";
+import ChangeToNIcon from "../../../assets/icons/changeToN";
 
 import { COLORS } from "../../../constants/color";
 
 const Header = () => {
   const [searchActive, setSearchActive] = useState<boolean>(false);
   const [searchInput, setSearchInput] = useState<string>("");
+  const [inputType, setInputType] = useState<string>("text");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const searchClickHandler = () => {
     setSearchActive(!searchActive);
   };
 
+  // handling search input
   function searchHandler(event: string) {
     setSearchInput(event.target.value);
   }
+
+  // change input type
+  function inputTypeHandler() {
+    if (inputType === "text") setInputType("number");
+    else setInputType("text");
+
+    setSearchInput("");
+  }
+
+  // focus input when user click search
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [searchActive, inputType]);
 
   return (
     <>
@@ -36,13 +54,32 @@ const Header = () => {
           </div>
         ) : (
           <div className="flex flex-grow gap-2 h-9 pl-2">
-            <div className="flex w-full items-center pl-2 rounded-full bg-white">
-              <SearchIcon stroke={COLORS.active} />
+            <div className="flex w-full items-center pl-2 rounded-full bg-white gap-2">
+              <button
+                onClick={inputTypeHandler}
+                className="bg-active text-white rounded-full px-2 flex justify-center items-center gap-1"
+              >
+                {inputType === "text" ? (
+                  <>
+                    <label>Alpha</label>
+                    <ChangeToNIcon fill="white" size={16} />
+                  </>
+                ) : (
+                  <>
+                    <label>Numb</label>
+                    <ChangeToAIcon fill="white" size={16} />
+                  </>
+                )}
+              </button>
+
               <input
+                ref={inputRef}
                 onChange={searchHandler}
+                value={searchInput}
+                name="search"
                 className="w-full mr-2 border-none focus:outline-none"
                 placeholder="Search..."
-                type="text"
+                type={inputType}
               ></input>
             </div>
 
