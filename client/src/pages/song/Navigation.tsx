3738532b6acc36bdc09d6669f761630dde7740bd
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-23 23:52:59 +0530

    fix return home button

--- client/src/pages/song/Navigation.tsx
+++ client/src/pages/song/Navigation.tsx
@@ -1,21 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
 import BackIcon from "../../assets/icons/back";
 import EditIcon from "../../assets/icons/edit";
 import StarIcon from "../../assets/icons/star";
 
-const Navigation = () => (
-  <nav className="flex w-full justify-between p-4 bg-amber-50">
-    <BackIcon fill="black" />
+const Navigation = () => {
+  const navigate = useNavigate();
 
-    <div className="flex gap-1 font-bold">
-      <p>001</p>
-      <p> PRAISE OUR CREATOR</p>
-    </div>
+  const navigateHomeHandler = () => navigate("/");
+  return (
+    <nav className="flex w-full justify-between p-4 bg-amber-50">
+      <button onClick={navigateHomeHandler}>
+        <BackIcon fill="black" />
+      </button>
 
-    <div className="flex gap-2 items-center">
-      <StarIcon />
-      <EditIcon fill="black" size={20} />
-    </div>
-  </nav>
-);
+      <div className="flex gap-1 font-bold">
+        <p>001</p>
+        <p> PRAISE OUR CREATOR</p>
+      </div>
+
+      <div className="flex gap-2 items-center">
+        <StarIcon />
+        <EditIcon fill="black" size={20} />
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
