d97176cdb879c05bfa9fd22a89e27665cb147617
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-24 22:32:48 +0530

    add feature-pwa feature

---
+++ client/src/registerSW.js
@@ -0,0 +1,13 @@
+// src/registerSW.js
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => {
+        console.log("✅ Service Worker registered:", reg);
+      })
+      .catch((err) => {
+        console.log("❌ SW registration failed:", err);
+      });
+  });
+}
