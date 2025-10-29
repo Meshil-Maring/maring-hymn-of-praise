507c5647b5ed39261c24323ff9d1eb04707bf819
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-10-29 23:04:12 +0530

    change song display design

--- client/src/pages/song/Navigation.tsx
+++ client/src/pages/song/Navigation.tsx
@@ -1,28 +1,25 @@
 import { useNavigate } from "react-router-dom";
 
 import BackIcon from "../../assets/icons/back";
-import EditIcon from "../../assets/icons/edit";
-import StarIcon from "../../assets/icons/star";
+import MenuIcon from "../../assets/icons/menu_dot";
 
 const Navigation = ({ id, title }: any) => {
   const navigate = useNavigate();
 
   const navigateHomeHandler = () => navigate("/");
+
   return (
-    <nav className="flex w-full justify-between p-4 bg-amber-50 ">
+    <nav className="flex w-full justify-between p-4 bg-bg-alpha shadow">
       <button onClick={navigateHomeHandler}>
         <BackIcon fill="black" />
       </button>
 
-      <div className="flex gap-1 font-bold text-center mx-4">
+      <div className="flex gap-1 text-sm font-bold text-center mx-4">
         <p>{id}.</p>
         <h1>{title}</h1>
       </div>
 
-      <div className="flex gap-2 items-center">
-        <StarIcon />
-        <EditIcon fill="black" size={20} />
-      </div>
+      <MenuIcon fill="black" />
     </nav>
   );
 };
